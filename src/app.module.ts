93942e27d5b6eb4b39f1c43406f6597d66a86a4a
YogaLee12 <s631693747@126.com>
2025-02-06 02:45:26 +1000

    backend init

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaService } from './prisma/prisma.service';
-import { ProductService } from './product/product.service';
+import { ProductService } from './products/product.service';
 
 @Module({
   imports: [],
